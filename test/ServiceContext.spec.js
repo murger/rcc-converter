b7a77ce09608d9fb71980851f0dc315fe3b09679
Gürhan Mermer <gmermer@gmail.com>
2019-10-11 13:45:29 +0300

    verbose testing

--- test/ServiceContext.spec.js
+++ test/ServiceContext.spec.js
@@ -3,26 +3,24 @@ import { ServiceProvider } from '../src/contexts/ServiceContext';
 const context = new ServiceProvider()
 const initialState = { base: 'GBP', rates: {}, loading: true, error: false }
 
-describe('context', () => {
-  it('should return the initial state', () => {
-    expect(context.state).toEqual(initialState)
-  })
+it('should return the initial state', () => {
+  expect(context.state).toEqual(initialState)
+})
 
-  it('should convert rates correctly', () => {
-    context.state = {
-      ...initialState,
-      rates: { 'USD': 1.2 }
-    }
+it('should convert rates correctly', () => {
+  context.state = {
+    ...initialState,
+    rates: { 'USD': 1.2 }
+  }
 
-    expect(context.getCurrencyRate('GBP', 'USD')).toEqual(1.2)
-  })
+  expect(context.getCurrencyRate('GBP', 'USD')).toEqual(1.2)
+})
 
-  it('should cross convert rates correctly', () => {
-    context.state = {
-      ...initialState,
-      rates: { 'USD': 1.2, 'JPY': 2.4 }
-    }
+it('should cross convert rates correctly', () => {
+  context.state = {
+    ...initialState,
+    rates: { 'USD': 1.2, 'JPY': 2.4 }
+  }
 
-    expect(context.getCurrencyRate('USD', 'JPY')).toEqual(2)
-  })
-});
+  expect(context.getCurrencyRate('USD', 'JPY')).toEqual(2)
+})
--- test/reducer.spec.js
+++ test/reducer.spec.js
@@ -1,35 +1,33 @@
 import reducer from '../src/components/Converter/reducer';
 
-describe('reducer', () => {
-  it('should return the initial state', () => {
-    expect(reducer({}, {})).toEqual({})
-  })
+it('should return the initial state', () => {
+  expect(reducer({}, {})).toEqual({})
+})
 
-  it('should handle WITHDRAW', () => {
-    const pockets = [
-      { currency: 'USD', amount: 20 }
-    ]
+it('should handle WITHDRAW', () => {
+  const pockets = [
+    { currency: 'USD', amount: 20 }
+  ]
 
-    const action = {
-      type: 'WITHDRAW',
-      currency: 'USD',
-      amount: 5
-    }
+  const action = {
+    type: 'WITHDRAW',
+    currency: 'USD',
+    amount: 5
+  }
 
-    expect(reducer(pockets, action)).toEqual([{ 'amount': 15, 'currency': 'USD' }])
-  })
+  expect(reducer(pockets, action)).toEqual([{ 'amount': 15, 'currency': 'USD' }])
+})
 
-  it('should handle DEPOSIT', () => {
-    const pockets = [
-      { currency: 'USD', amount: 20 }
-    ]
+it('should handle DEPOSIT', () => {
+  const pockets = [
+    { currency: 'USD', amount: 20 }
+  ]
 
-    const action = {
-      type: 'DEPOSIT',
-      currency: 'USD',
-      amount: 5
-    }
+  const action = {
+    type: 'DEPOSIT',
+    currency: 'USD',
+    amount: 5
+  }
 
-    expect(reducer(pockets, action)).toEqual([{ 'amount': 25, 'currency': 'USD' }])
-  })
-});
+  expect(reducer(pockets, action)).toEqual([{ 'amount': 25, 'currency': 'USD' }])
+})
